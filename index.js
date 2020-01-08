1b27921fdafb16c67d39acc08a84304e30508d5d
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-08 08:33:23 -0500

    correct url, request works

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const searchUrl = "https://api.github.com/users"
+// const searchUrl = "https://api.github.com/users"
 
-function formatRequest(params){
-  const queryItems = Object.keys(params).map(
-    objectKeys => `${objectKeys}=${params[objectKeys]}` 
-  )
-  return queryItems.join('&');
-}
+const searchUrl = "https://api.github.com/users/${search}/repos"
+
+// function formatRequest(params){
+//   const queryItems = Object.keys(params).map(
+//     objectKeys => `${objectKeys}=${params[objectKeys]}` 
+//   )
+//   return queryItems.join('&');
+// }
 
 
 function getResults(search){
-  const params = {
-    q: search,
-    language: "en",
-  };
+//   const params = {
+//     q: search,
+//   };
   
-  const newString = formatRequest(params);
-  const url = searchUrl + "?" + newString;
+//   const newString = formatRequest(params);
+  const url =  `https://api.github.com/users/${search}/repos`
+
+//   + "?" + newString;
   console.log(url);
   fetch(url)
   .then(response => response.json())
