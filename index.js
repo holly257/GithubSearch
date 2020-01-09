f90e3cb80733309566654234db52ed8539009565
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-09 10:54:01 -0500

    cleaned up js

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-// const searchUrl = "https://api.github.com/users"
 
 const searchUrl = "https://api.github.com/users/${search}/repos"
 
-// function formatRequest(params){
-//   const queryItems = Object.keys(params).map(
-//     objectKeys => `${objectKeys}=${params[objectKeys]}` 
-//   )
-//   return queryItems.join('&');
-// }
-
-
 function getResults(search){
-//   const params = {
-//     q: search,
-//   };
-  
-//   const newString = formatRequest(params);
   const url =  `https://api.github.com/users/${search}/repos`
-
-//   + "?" + newString;
   console.log(url);
   fetch(url)
   .then(response => response.json())
